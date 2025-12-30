24942be2be4c58f7bd31412adcae99ce25db2031
selimoren <selimoren235@gmail.com>
2025-12-30 21:19:23 +0300

    radon ve vulture düzeltildi

--- admin.py
+++ admin.py
@@ -1,5 +1,91 @@
+"""
+Docstring for admin
+"""
+
 from utils import dangerous_eval
 
+"""
+Admin modülü, yönetici paneli ve ilgili işlevleri içerir.
+"""
 
 def admin_panel(code):
     return dangerous_eval(code)
+
+def complex_calculation(x, y, z):
+    """
+    Karmaşık bir hesaplama fonksiyonu - Radon analizi için yüksek CC
+    """
+    if x > 0:
+        if y > 0:
+            if z > 0:
+                return x + y + z
+            elif z < 0:
+                return x + y - z
+            else:
+                return x + y
+        elif y < 0:
+            if z > 0:
+                return x - y + z
+            elif z < 0:
+                return x - y - z
+            else:
+                return x - y
+        else:
+            if z > 0:
+                return x + z
+            elif z < 0:
+                return x - z
+            else:
+                return x
+    elif x < 0:
+        if y > 0:
+            if z > 0:
+                return -x + y + z
+            elif z < 0:
+                return -x + y - z
+            else:
+                return -x + y
+        elif y < 0:
+            if z > 0:
+                return -x - y + z
+            elif z < 0:
+                return -x - y - z
+            else:
+                return -x - y
+        else:
+            if z > 0:
+                return -x + z
+            elif z < 0:
+                return -x - z
+            else:
+                return -x
+    else:
+        if y > 0:
+            if z > 0:
+                return y + z
+            elif z < 0:
+                return y - z
+            else:
+                return y
+        elif y < 0:
+            if z > 0:
+                return -y + z
+            elif z < 0:
+                return -y - z
+            else:
+                return -y
+        else:
+            if z > 0:
+                return z
+            elif z < 0:
+                return -z
+            else:
+                return 0
+
+def unused_function():
+    """
+    Kullanılmayan fonksiyon - Vulture analizi için
+    """
+    a = 1
+    b = 2
+    return a + b
--- utils.py
+++ utils.py
@@ -1,15 +1,19 @@
 import os
+import subprocess
+from typing import List
+import ast
 
+ALLOWED_COMMANDS = {
+    "python_version": ["python", "--version"],
+}
 
-def run_command(cmd):
-    # ❌ os.system
-    os.system(cmd)
+def run_command(key):
+    args = ALLOWED_COMMANDS[key]
+    result = subprocess.run(args, check=True, capture_output=True, text=True, shell=False)
+    return result.stdout
 
-
-def dangerous_eval(expr):
-    # ❌ eval
-    return eval(expr)
-
+def calculate(expr):
+    return ast.literal_eval(expr)
 
 def unused_helper():
     pass
